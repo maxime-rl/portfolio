70bee29bc1bd13ab3df8713e87b178f1283b29f4
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-12-25 03:59:53 +0100

    feat: add HomePage with Nav component
    build: add Lit lib for better management of literal templates and have a better development experience

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 import "./assets/style/index.css";
+import { Navbar } from "./components";
+import { HomePage } from "./pages/HomePage";
+
+window.customElements.define("navbar-component", Navbar);
+window.customElements.define("home-page", HomePage);
+
+document.body.insertAdjacentHTML("afterbegin", "<home-page></home-page>");
